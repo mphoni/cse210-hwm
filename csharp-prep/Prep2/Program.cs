ef7086559797b1e0c8f984c9f58bc4384c223205
mphoni <131215278+mphoni@users.noreply.github.com>
2023-04-21 23:30:34 -0600

    Update Program.cs

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,42 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.Write("what is your grade percentage?");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string grade = "";
+
+        if (percent >= 90)
+        {
+            grade = "A";
+        }
+        else if (percent >= 80)
+        {
+            grade = "B";
+        }
+        else if (percent >= 70)
+        {
+           grade = "C";
+        }
+        else if (percent >= 60)
+        {
+            grade= "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {grade}");
+        
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congratulations");
+        }
+        else
+        {
+            Console.WriteLine("Fail!");
+        }
     }
 }
