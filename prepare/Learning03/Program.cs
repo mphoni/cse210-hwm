85a064396aa69966aefe2a5271d72b188ac41833
mphoni <131215278+mphoni@users.noreply.github.com>
2023-05-14 00:01:25 -0600

    Add files via upload

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,97 +1,38 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+namespace Develop02
 {
-    static void Main(string[] args)
+    class Program
     {
-      Journal journal = new Journal();
-        journal._storeEntry = "";
-        journal._saveEntry = "";
-        journal._loadEntry = "";
-        journal.Display();
-        
-        Prompter prompter = new Prompter();
-        prompter._prompts = "";
-        prompter.Display();
-        
-        Entry entry1 = new Entry();
-        entry1._notes = "what would you like to do";
-        entry1._response = "write your response";
-
-        Entry entry2 = new Entry();
-        entry2._notes = "what was the best part of your day?";
-        entry2._response = "write your response";
-        List<Entry> enter = new List<Entry>();
-        enter.Add(entry1);
-        enter.Add(entry2);
-
-        journal._entries.Add(entry1);
-        journal._entries.Add(entry2);
-
-        foreach(Entry entry in enter)
+        static void Main(string[] args)
         {
-        Console.WriteLine(entry2._notes);
+            Console.WriteLine("Hello Learning04 World!");
+
+            Entry myEntry = new Entry();
+            myEntry.Store("What is your name?","mfoniso martin", "8 may 2023");
+
+            Journal journal1 = new Journal();
+            journal1.StoreEntry(myEntry);
+
         
-       }
- 
-        SaveToFile(enter);
-       
 
-      List<Entry> newEnteries= ReadFromFile();
-      foreach(Entry entry in enter)
-      {
-        Console.WriteLine(entry._response);
-      }
-
-       
-
-    }
-
-     public void Display(){
-            
-        DateTime theCurrentTime = DateTime.Now;
-        string dateText = theCurrentTime.ToShortDateString();
-
-        Console.WriteLine($"{dateText}");
-        }
-
-    public static void SaveToFile(List<Entry> enter)
-    {
-        Console.WriteLine("savimg to file...");
-        string filename = "enter.txt";
-
-        using (StreamWriter outputFile = new StreamWriter(filename))
-        {
-            foreach(Entry entry in enter)
+            List<Entry> entries = journal1.GetAllEntries();
+            foreach(Entry entry in entries)
             {
-                outputFile.WriteLine($"{entry._notes}~~{entry._response} ");
+                string message = entry.GetAsString();
+                Console.WriteLine(message);
             }
+
+            Prompter prompt1 = new Prompter();
+            prompt1.Display();
+            prompt1.DisplayQuestions();
+            
+            Prompts prompt = new Prompts();
+            prompt.DisplayQuestions();
+            prompt.DisplayQuestion();
+
+
         }
-    } 
-    
-    public static List<Entry> ReadFromFile()
-    {
-        Console.WriteLine("reading list form file...");
-        List<Entry> enter = new List<Entry>();
-        string filename = "enter.txt";
-
-
-        string[] lines = System.IO.File.ReadAllLines(filename);
-
-    foreach(string line in lines)
-    {
-        //Console.WriteLine(lines);
-
-        string[] parts = line.Split("~~");
-
-
-         Entry newEnteries = new Entry();
-         newEnteries._notes = parts[0];
-         newEnteries._response = parts[1];
-
-        enter.Add(newEnteries);
-    }
-     return enter;
-        
     }
 }
