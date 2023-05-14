e3e47a5c83e149a63be661bdcf0558e3c49dccd0
mphoni <131215278+mphoni@users.noreply.github.com>
2023-05-13 23:47:35 -0600

    Update Prompter.cs

--- prepare/Learning03/Prompter.cs
+++ prepare/Learning03/Prompter.cs
@@ -1,10 +1,44 @@
 using System;
+using System.Collections.Generic;
 
-public class Prompter{
+namespace Develop02
+{
 
-    public string _prompts;
+    public class Prompter
+    {
+        public string question;
+        public string question1;
+        public string question2;
+        public string question3;
+        public string question4;
+        public List<Prompter> questions;
+ 
+       public Prompter()
+       {
+            questions = new List<Prompter>();
+       }
 
-     public void Display(){
-        Console.WriteLine($"please select one of the following");
+        public List<Prompter> GetQuestions()
+        {
+            return questions;
+        }
+        
+        public void DisplayQuestions()
+        {
+            Console.WriteLine("What was the strongest emotion I felt today?");
+                string question = Console.ReadLine();
+        
+            Console.WriteLine("If I had one thing I could do over today, what would it be?"); 
+                string question1 = Console.ReadLine();
+
+            Console.WriteLine("What was the best part of my day?");
+                string question2 = Console.ReadLine();
+            
+            Console.WriteLine("How did I see the hand of the Lord in my life today?");
+                string question3 = Console.ReadLine();
+             
+            Console.WriteLine("Who was the most interesting person I interacted with today?");
+                 string question4 = Console.ReadLine();
+        }
     }
 }
