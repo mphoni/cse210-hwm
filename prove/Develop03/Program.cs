6b45172553168ab082e971e54d558ee05a5459e2
mphoni <131215278+mphoni@users.noreply.github.com>
2023-06-12 14:52:53 -0600

    Update Program.cs

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,60 @@
 using System;
 
-class Program
+namespace Develop03
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Develop03 World!");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Learning05 World!");
+        
+            Loader loader = new Loader("dictionary.csv");
+            Scripture scripture = new Scripture(loader.FindRandomScripture());
+            Console.Clear();
+
+            while (scripture.hiddenWords != scripture.totalWords)
+            {
+
+                Console.WriteLine(loader.reference + " " + scripture.AsString());
+                Console.WriteLine();
+                Console.WriteLine("Press P to hide a word.");
+                Console.WriteLine("Press O to reveal the last word.");
+                Console.WriteLine("Type 'quit' to exit.");
+                string keyEnter = Console.ReadLine();
+                    
+                if (keyEnter.ToLower() == "p")
+                {
+                    scripture.HideRandomWord();
+                    Console.Clear();
+                }
+                    
+                else if (keyEnter.ToLower() == "o")
+                {
+                    scripture.ShowLastHiddenWord();
+                    Console.Clear();
+                }
+                    
+                else if (keyEnter.ToLower() == "quit")
+                {
+                    break;
+                }
+                    
+
+                    
+                else 
+                {
+                Console.Clear();
+                Console.WriteLine("Please enter P, O, or 'quit'.");
+                Console.WriteLine();
+                }
+            }
+                Console.Clear();
+                Console.WriteLine(loader.reference + " " + scripture.AsString());
+                Console.WriteLine();
+                Console.WriteLine("Press any key to terminate program.");
+                Console.ReadLine();
+
+        
+        }
     }
 }
